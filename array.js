49bf0ab32dd06dab9a7cf3c623639883f448f520
Mayank Agarwal <68071667+agarwalm92@users.noreply.github.com>
2023-07-24 00:14:15 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,34 @@
+//23072023
+
+const fruits = ["apple","banana","grapes"];
+const fruit = "";
+
+//// PRINT ARRAY
+////-------------
+//console.log(fruits);
+//console.log(fruits[0]);
+//console.log(fruits.indexOf("apple"));
+
+//// ADD TO ARRAY
+////------------
+
+////specific position
+//fruits[3]="mango" 
+
+////last
+//fruits.push("mango");
+
+////first
+//fruits.unshift("strawberry");
+
+////REMOVE FROM ARRAY
+////------------------
+//fruits.pop(); //last
+//fruits.pop(1); //index
+
+////CHECK ARRAY
+////-------------
+//console.log(Array.isArray(fruit));
+
+
+console.log(fruits);
